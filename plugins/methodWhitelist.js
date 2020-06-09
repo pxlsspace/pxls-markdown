ea7d55925e72aa016d0335dcb3a55b57931188ab
Martín (Netux) Rodríguez <send.email+git@netux.site>
2020-06-09 15:05:02 -0300

    methodWhitelist: make options optional

--- plugins/methodWhitelist.js
+++ plugins/methodWhitelist.js
@@ -1,23 +1,31 @@
 module.exports = function(opts) {
 	const parserProto = this.Parser.prototype;
 
-	for (let idx = parserProto.blockMethods.length - 1; idx >= 0; idx--) {
-		if (!opts.aggresive && parserProto.blockMethods[idx] === 'paragraph') {
-			continue;
-		}
+	if (!opts) {
+		return;
+	}
 
-		if (!opts.block.includes(parserProto.blockMethods[idx])) {
-			parserProto.blockMethods.splice(idx, 1);
+	if (opts.block != null) {
+		for (let idx = parserProto.blockMethods.length - 1; idx >= 0; idx--) {
+			if (!opts.aggresive && parserProto.blockMethods[idx] === 'paragraph') {
+				continue;
+			}
+
+			if (!opts.block.includes(parserProto.blockMethods[idx])) {
+				parserProto.blockMethods.splice(idx, 1);
+			}
 		}
 	}
 
-	for (let idx = parserProto.inlineMethods.length - 1; idx >= 0; idx--) {
-		if (!opts.aggresive && parserProto.inlineMethods[idx] === 'text') {
-			continue;
-		}
+	if (opts.inline != null) {
+		for (let idx = parserProto.inlineMethods.length - 1; idx >= 0; idx--) {
+			if (!opts.aggresive && parserProto.inlineMethods[idx] === 'text') {
+				continue;
+			}
 
-		if (!opts.inline.includes(parserProto.inlineMethods[idx])) {
-			parserProto.inlineMethods.splice(idx, 1);
+			if (!opts.inline.includes(parserProto.inlineMethods[idx])) {
+				parserProto.inlineMethods.splice(idx, 1);
+			}
 		}
 	}
 };
